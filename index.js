bcb103ab9efb71e593c8a139f3cef50dddef10cc
himanganimesh2 <himanganimesh2@gmail.com>
2023-04-29 14:14:08 +0530

    auth route is created

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require("express");
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const authRoute = require('./routes/auth')
 
 dotenv.config();
+app.use(express.json());
 
 mongoose.connect(
     process.env.MONGO_URL)
-    .then(console.log('connected to MongoDB'))
+    .then(console.log('connected to the MongoDB'))
     .catch((err)=>console.log(err));
 
 
-app.use("/", (req, res)=>{
-    console.log("hey this is the main url");
-})
+app.use("/api/auth", authRoute);
 
 app.listen("5000", ()=>{
     console.log("backend is running");
